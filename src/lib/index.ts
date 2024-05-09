c1e858ba4efbf165f2239a4e2b23a950de786219
Brendan Dash <me@aiwan.run>
2024-05-09 22:39:15 +0800

    fix: type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,7 @@ import type { ClassValue } from "clsx";
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
-function cn(inputs: ClassValue) {
+function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
