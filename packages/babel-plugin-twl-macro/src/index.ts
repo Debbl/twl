9420b647ba72591570b617289973d4339864e2e9
Brendan Dash <me@aiwan.run>
2025-08-08 10:46:32 +0800

    feat: wip

--- packages/babel-plugin-twl-macro/src/index.ts
+++ packages/babel-plugin-twl-macro/src/index.ts
@@ -1,3 +1,7 @@
-import cls from "./macro";
+import cls from "./cls.macro";
 
+const result = cls`text-sm font-bold bg-sky-500`;
+
+// eslint-disable-next-line no-console
+console.log("🚀 ~ result:", result);
 export { cls };
