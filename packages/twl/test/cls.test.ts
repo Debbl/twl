cbb244080800ac20f7e1ebce82f3d31ee2d9df7d
Brendan Dash <me@aiwan.run>
2026-02-10 23:41:24 +0800

    chore: add tsdown dependency and update pnpm-lock.yaml; remove unused tsup.config.ts

--- packages/twl/test/cls.test.ts
+++ packages/twl/test/cls.test.ts
@@ -1,40 +1,40 @@
-import { expect, it } from "vitest";
-import { cls } from "../src";
+import { expect, it } from 'vitest'
+import { cls } from '../src'
 
-it("basic", () => {
-  const result = cls`text-sm`;
-  expect(result).toMatchInlineSnapshot(`"text-sm"`);
-});
+it('basic', () => {
+  const result = cls`text-sm`
+  expect(result).toMatchInlineSnapshot(`"text-sm"`)
+})
 
-it("basic with expressions", () => {
-  const result = cls`text-sm ${"font-bold"}   ${"bg-sky-500"}`;
-  expect(result).toMatchInlineSnapshot(`"text-sm font-bold bg-sky-500"`);
-});
+it('basic with expressions', () => {
+  const result = cls`text-sm ${'font-bold'}   ${'bg-sky-500'}`
+  expect(result).toMatchInlineSnapshot(`"text-sm font-bold bg-sky-500"`)
+})
 
-it("multiple lines", () => {
+it('multiple lines', () => {
   const result = cls`
     text-sm              bg-sky-500
     font-bold
-  `;
-  expect(result).toMatchInlineSnapshot(`"text-sm bg-sky-500 font-bold"`);
-});
+  `
+  expect(result).toMatchInlineSnapshot(`"text-sm bg-sky-500 font-bold"`)
+})
 
-it("multiple lines with expressions", () => {
+it('multiple lines with expressions', () => {
   const result = cls`
     text-sm              bg-sky-500
-    font-bold ${"font-bold"}   ${"bg-sky-500"}
-  `;
+    font-bold ${'font-bold'}   ${'bg-sky-500'}
+  `
   expect(result).toMatchInlineSnapshot(
     `"text-sm bg-sky-500 font-bold font-bold bg-sky-500"`,
-  );
-});
+  )
+})
 
-it("multiple lines with comments", () => {
+it('multiple lines with comments', () => {
   const result = cls`
     // hello
     text-sm              bg-sky-500
     // world
     font-bold
-  `;
-  expect(result).toMatchInlineSnapshot(`"text-sm bg-sky-500 font-bold"`);
-});
+  `
+  expect(result).toMatchInlineSnapshot(`"text-sm bg-sky-500 font-bold"`)
+})
